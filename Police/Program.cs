37ffcdac3482d631042cf89fa0108ee0b22a95b4
Мария Тарлавина <tarlavina50@mail.ru>
2024-01-18 16:31:53 +0700

    coding dz

---
+++ Police/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Police
+{
+    internal class Program
+    {
+        SqlConnection connection;
+        SqlCommand cmd;
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+            Console.WriteLine(connectionString);
+        }
+    }
+}
