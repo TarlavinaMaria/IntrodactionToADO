3e89dab389bb37b69734212750f852e199ad7bbb
Мария Тарлавина <tarlavina50@mail.ru>
2024-01-19 15:54:07 +0700

    coding dz

--- Academy/Schedule.cs
+++ Academy/Schedule.cs
@@ -79,18 +79,18 @@ namespace Academy
             {
                 connection.Open();
                 string discipline_schedule_name = discipline_select.Text;
-                string discipline_id = "SELECT discipline_id FROM Disciplines WHERE discipline_name LIKE @discipline_schedule_name";
+                string discipline_id = $"SELECT discipline_id FROM Disciplines WHERE discipline_name LIKE @discipline_schedule_name";
                 SqlCommand discipline_idCommand = new SqlCommand(discipline_id, connection);
                 discipline_idCommand.Parameters.AddWithValue("@discipline_schedule_name", discipline_id);
-                //reader = discipline_idCommand.ExecuteReader();
-                //reader.Close();
+                reader = discipline_idCommand.ExecuteReader();
+                reader.Close();
 
                 string teacher_schedule_name = teacher_select.Text;
                 string teacher_id = "SELECT teacher_id FROM Teachers WHERE last_name LIKE @teacher_schedule_name";
                 SqlCommand teacher_idCommand = new SqlCommand(teacher_id, connection);
                 teacher_idCommand.Parameters.AddWithValue("@teacher_schedule_name", teacher_id);
-                //reader = teacher_idCommand.ExecuteReader();
-                //reader.Close();
+                reader = teacher_idCommand.ExecuteReader();
+                reader.Close();
 
                 string date = date_select.Text;
                 string time = time_select.Text;
@@ -99,15 +99,15 @@ namespace Academy
                 string group_id = "SELECT group_id FROM Groups WHERE group_name LIKE @group_schedule_name";
                 SqlCommand group_idCommand = new SqlCommand(group_id, connection);
                 group_idCommand.Parameters.AddWithValue("@group_schedule_name", group_id);
-                //reader = group_idCommand.ExecuteReader();
-                //reader.Close();
+                reader = group_idCommand.ExecuteReader();
+                reader.Close();
 
                 int interval = 2;
                 string number_of_lessons = "SELECT number_of_lessons FROM Disciplines WHERE discipline_id = @discipline_id";
                 SqlCommand number_of_lessons_idCommand = new SqlCommand(number_of_lessons, connection);
-                number_of_lessons_idCommand.Parameters.AddWithValue("@discipline_id", group_id);
-                //reader = number_of_lessons_idCommand.ExecuteReader();
-                //reader.Close();
+                number_of_lessons_idCommand.Parameters.AddWithValue("@discipline_id", discipline_id);
+                reader = number_of_lessons_idCommand.ExecuteReader();
+                reader.Close();
 
                 int counter = 0;
 
@@ -126,7 +126,7 @@ namespace Academy
                     //    Console.WriteLine("Запись уже существует.");
                     //    return;
                     //}
-                    string query = "INSERT INTO Schedule (discipline, teacher, [date], [time], spent, [group]) VALUES (@discipline_id, @teacher_id, @date, @time, @date < GETDATE() ? 1 : 0), @group_id)";
+                    string query = "INSERT INTO Schedule (discipline, teacher, date, [time], spent, [group]) VALUES (@discipline_id, @teacher_id, @date, @time, @date < GETDATE() ? 1 : 0, @group_id)";
                     SqlCommand cmd = new SqlCommand(query, connection);
                     cmd.Parameters.AddWithValue("@discipline_id", discipline_id);
                     cmd.Parameters.AddWithValue("@teacher_id", teacher_id);
@@ -148,7 +148,7 @@ namespace Academy
                     //    Console.WriteLine("Запись уже существует.");
                     //    return;
                     //}
-                    string query2 = "INSERT INTO Schedule (discipline, teacher, [date], [time], spent, [group]) VALUES (@discipline_id, @teacher_id, @date, @time, @date < GETDATE() ? 1 : 0), @group_id)";
+                    string query2 = "INSERT INTO Schedule (discipline, teacher, [date], [time], spent, [group]) VALUES (@discipline_id, @teacher_id, @date, @time, @date < GETDATE() ? 1 : 0, @group_id)";
                     SqlCommand cmd2 = new SqlCommand(query2, connection);
                     cmd2.Parameters.AddWithValue("@discipline_id", discipline_id);
                     cmd2.Parameters.AddWithValue("@teacher_id", teacher_id);
