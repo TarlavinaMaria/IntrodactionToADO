168f6a397e4125c584dcdae9962e5f187ffd04e1
Мария Тарлавина <tarlavina50@mail.ru>
2024-01-24 16:10:42 +0700

    coding dz

--- Academy/StudentInfo.cs
+++ Academy/StudentInfo.cs
@@ -30,6 +30,9 @@ namespace Academy
             connection = new SqlConnection(connectionString);
             CellValue = data;
             Load_StudentInfo(data);
+            Load_Group(data);
+            Load_Spec(data);
+            Load_Attendance(data);
         }
         void Load_StudentInfo(string last_name)
         {
@@ -46,10 +49,94 @@ namespace Academy
                 }
                 reader.Close();
                 connection.Close();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(this, exception.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (connection != null) connection.Close();
+                if (reader != null) reader.Close();
+            }
+        }
+        void Load_Group(string last_name)
+        {
+            try
+            {
+                connection.Open();
+                string FullName = @"SELECT group_name FROM Students, Groups WHERE last_name = @last_name AND [group] = group_id";
+                SqlCommand cmd = new SqlCommand(FullName, connection);
+                cmd.Parameters.AddWithValue("@last_name", last_name);
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    rtbGroup.Text = $"{reader[0]}";
+                }
+                reader.Close();
+                connection.Close();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(this, exception.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (connection != null) connection.Close();
+                if (reader != null) reader.Close();
+            }
+        }
+        void Load_Spec(string last_name)
+        {
+            try
+            {
+                connection.Open();
+                string spec = @"SELECT direction_name FROM Students, Groups, Directions WHERE last_name = @last_name AND [group] = group_id AND direction = direction_id";
+                SqlCommand cmd = new SqlCommand(spec, connection);
+                cmd.Parameters.AddWithValue("@last_name", last_name);
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    rtbSpec.Text = $"{reader[0]}";
+                }
+                reader.Close();
+                connection.Close();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(this, exception.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (connection != null) connection.Close();
+                if (reader != null) reader.Close();
+            }
+        }
+        void Load_Attendance(string last_name)
+        {
+            try
+            {
+                connection.Open();
+                string count_all = "SELECT (CAST(COUNT(CASE WHEN present = 1 THEN 1 END) AS float) / COUNT(present)) * 100.0 AS AttendancePercentage" +
+                    " FROM Students, Attendance WHERE last_name = @last_name AND stud_id = student";
+                SqlCommand cmd = new SqlCommand(count_all, connection);
+                cmd.Parameters.AddWithValue("@last_name", last_name);
+                reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    double attendancePercentage = Convert.ToDouble(reader["AttendancePercentage"]);
+                    if(attendancePercentage > 0)
+                    {
+                        rtbAttendance.Text = $"{attendancePercentage}%";
+                    }
+                    else
+                    {
+                        rtbAttendance.Text = $"0%";
+                    }
+                }
 
-                //rtbFullName.Text = last_name;
-
-
+                reader.Close();
+                connection.Close();
             }
             catch (Exception exception)
             {
